fff8d0292d46c29887c9372ed4e906c79d711b91
Wojciech Skirlo <wojciechskiro@gmail.com>
2020-11-23 20:23:49 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>ProjectQ12 - Login</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <main>
+        <section id="left-container">
+            <h2>New here</h2>
+        </section>
+        <section id="right-container">
+            <div class="sign-in">
+                <h2>Sign in</h2>
+                <form action="" method="POST" class="sign-in-form"></form>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
