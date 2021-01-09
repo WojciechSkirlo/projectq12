2aa4b348a888feee8009fba0255f51b12810b218
Wojciech Skirlo <wojciechskiro@gmail.com>
2021-01-09 18:11:13 +0100

    Fixes

--- user.php
+++ user.php
@@ -293,10 +293,89 @@ try {
                 </div>
             </div>
         </div>
+        <div class="wrapper">
+            <?php
+            require_once "connect.php";
+            try {
+                $link = new mysqli($db_server, $db_login, $db_password, $db_name);
+                if ($link->connect_errno != 0) {
+                    throw new Exception(mysqli_connect_errno());
+                } else {
+                    $results_per_page = 7;
+
+                    $result = $link->query("SELECT id FROM quotes WHERE user_id='$user_id'");
+                    $how_many_quote = $result->num_rows;
+
+                    $number_of_pages = ceil($how_many_quote / $results_per_page);
+
+                    if (!isset($_GET['page'])) {
+                        $page = 1;
+                    } else {
+                        $page = $_GET['page'];
+                    }
+                    $this_page_first_result = ($page - 1) * $results_per_page;
+
+                    $result = $link->query("SELECT quotes.*, quotes.id AS 'quotes_id', authors.*, categories.name AS 'name_category', users.*, users.id AS 'user_id' FROM quotes INNER JOIN categories ON quotes.categories_id=categories.id INNER JOIN authors ON quotes.author_id=authors.id INNER JOIN users ON quotes.user_id=users.id WHERE quotes.user_id='$user_id' ORDER BY quotes.creation_date DESC LIMIT " . $this_page_first_result . ', ' . $results_per_page);
+                    if (!$result) {
+                        throw new Exception($link->error);
+                    }
+                    $how_many = $result->num_rows;
+                    if ($how_many > 0) {
+                        echo "<h3 class='info'>Author's quotes</h3>";
+                        echo '<div class="wrapper-box">';
+                        while ($row = $result->fetch_assoc()) {
+                            echo '<div class="box">';
+                            echo '<div class="img-box">';
+                            echo '<img src="' . $row['img_quote'] . '" />';
+                            echo '</div>';
+                            echo '<div class="background-box"></div>';
+                            echo '<a href="quote.php?quote_id=' . $row['quotes_id'] . '"><div class="text-box">';
+                            if (strlen($row['text_quote']) < 50) {
+                                echo '<h2>„' . $row['text_quote'] . '” - <span class="gradient">' . $row['name'] . " " . $row['surname'] . '<span></h2>';
+                            } else if (strlen($row['text_quote']) < 100) {
+                                echo '<h3>„' . $row['text_quote'] . '” - <span class="gradient">' . $row['name'] . " " . $row['surname'] . '<span></h3>';
+                            } else if (strlen($row['text_quote']) < 225) {
+                                echo '<h4>„' . $row['text_quote'] . '” - <span class="gradient">' . $row['name'] . " " . $row['surname'] . '<span></h4>';
+                            } else {
+                                echo '<h5>„' . $row['text_quote'] . '” - <span class="gradient">' . $row['name'] . " " . $row['surname'] . '<span></h5>';
+                            }
+                            echo '</div>';
+                            echo '</a>';
+                            echo '<div class="add-box">';
+                            echo '<span class="info">Added by: <a href=user.php?id_user=' . $row['user_id'] . '>' . $row['user'] . '</a></span>';
+                            echo '<span class="info">Category: <a href=category.php?id_category=' . $row['categories_id'] . '>' . $row['name_category'] . '</a></span>';
+                            echo '<span class="info">Author: <a href=author.php?id_author=' . $row['author_id'] . '>' . $row['name'] . " " . $row['surname'] . '</a></span>';
+                            echo '</div>';
+                            echo '</div>';
+                        }
+                        echo '</div>';
+                    }
+                    $link->close();
+                }
+            } catch (Exception $e) {
+                echo "Server error! Sorry :/";
+                echo "<br/> Information for the developer: " . $e;
+            }
+            ?>
+        </div>
     </section>
 
     <?php
+    if ($number_of_pages > 1) {
+        echo '<div id="pagination">';
+        for ($page = 1; $page <= $number_of_pages; $page++) {
+            if ($number_of_pages != 1) {
+                echo '<div class="pagination-page"><a href="user.php?id_user=' . $user_id . '&page=' . $page . '" class="';
+                if ($page == $_GET['page']) {
+                    echo "active";
+                }
+                echo '">' . $page . '</a></div>';
+            }
+        }
+        echo '</div>';
+    }
     ?>
+
     <section id="authors">
         <div class="wrapper">
             <div class="box"></div>
